55da5d28ccc2d99d4f4b156014a4892883b908a8
Beracira <g.beracira@gmail.com>
2015-06-14 08:06:31 -0400

    album included

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from netease_api import search
 from pprint import pprint
 import netease_api
 import os
+import sys
 
 welcome = """Hello! This is a open-source third party NetEase Music player!"""
 
@@ -10,11 +11,13 @@ print (welcome)
 while(True):
 	print("What are you going to do..?")
 	print("1. Search for songs")
-	print("2. Listen to author's playlist")
+	print("2. Search for album")
+	print("3. Listen to author's playlist")
+	print("4. Exit")
 	opt = input()
 	if (opt == '1'):
 		name = input("Enter name of your song: ")
-		result = search(name.encode('utf8'), stype = 1)
+		result = search(name.encode('utf8'), stype = 1)['songs']
 		if (result == 'Search Failed!'):
 			print (result + 'Are you connected to the Internet?')
 		else:
@@ -32,5 +35,31 @@ while(True):
 				# print ("{name}\t\t{album}\t\t{artists}".format(**song))
 				temp += [song]
 			song_id = temp[int(input("Enter your choice: "))]['id']
-			netease_api.play(song_id)
+			netease_api.download(song_id)
+
+	if (opt == '2'):
+		name = input("Enter name of your album: ")
+		result = search(name.encode('utf8'), stype = 10)['albums']
+		if (result == 'Search Failed!'):
+			print (result + 'Are you connected to the Internet?')
+		else:
+			temp = []
+			print ("   {:<24} {:<24} {:>6}".format("Name","Artists","Size"))
+			for (album, i) in zip(result, range(0, 10)):
+				album = {
+					'name': album['name'],
+					'artist': album['artist']['name'],
+					'size': album['size'],
+					'id': album['id']}
+				print (str(i) + ". {name:<24} {artist:<24} {size:>6}".format(**album))
+				temp += [album]
+			album_id = temp[int(input("Enter your choice: "))]['id']
+			netease_api.download_by_album(album_id)
+
+	if (opt == '3'):
+		pass
+
+	if (opt == '4'):
+		sys.exit(0)
+				
 
