9b8861b305eafc508f9716cce3cbb405db041e96
Beracira <g.beracira@gmail.com>
2015-06-20 23:35:55 -0400

    ui file added

---
+++ convert.py
@@ -0,0 +1,12 @@
+from PyQt5 import uic
+# from PyQt5.QtCore import PYQT_VERSION_STR
+import PyQt5 
+
+fin = open("main.ui", "r")
+fout = open("ui.py", "w")
+# fout.write("fuck")
+
+uic.compileUi(fin, fout)
+
+
+# print (PYQT_VERSION_STR)
